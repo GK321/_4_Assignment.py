997476b52376877d7f9b2bf514679b2d7e9f3a7c
Gajanan Kabure <117345006+GK321@users.noreply.github.com>
2022-12-03 19:28:34 +0530

    Add files via upload
    Assignment Number :- 4
    Question Number :- 1

---
+++ _1_Question.py
@@ -0,0 +1,8 @@
+# Write a Python program to create a lambda function that adds 25 to a given number passed in as an argument.
+
+# sample input: 10
+
+r = lambda a : a + 25
+print(r(10))
+r = lambda x, y : x * y
+print(r(7, 5))
